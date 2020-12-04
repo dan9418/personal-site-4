9cc2840dad126672562e8a1169a35af9d8f2a615
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-12-03 21:11:55 -0800

    reset

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,58 +2,13 @@
 import React, { useState } from 'react';
 import 'react-hot-loader';
 import { hot } from 'react-hot-loader/root';
-import About from '../../../react/src/tabs/about/About';
-import Docs from '../../../react/src/tabs/docs/DocsTab';
-import Explore from '../../../react/src/tabs/explore/ExploreTab';
-import * as Icons from '../img/Icons';
-import './NavBar.css';
-import './Stage.css';
 
-const TABS = [
-	{
-		id: 'about',
-		name: 'About',
-		component: <About />
-	},
-	{
-		id: 'explore',
-		name: 'Explore',
-		component: <Explore />
-	},
-	{
-		id: 'docs',
-		name: 'Docs',
-		component: <Docs />
-	}
-];
+import './Main.css';
 
 const Main = () => {
-	const [tabIndex, setTabIndex] = useState(0);
-	const tab = TABS[tabIndex];
 	return (
 		<>
-			<nav className="top-nav pw-primary">
-				<div className="logo">Play What?</div>
-				{TABS.map((t, i) => (
-					<div
-						className={`tab ${tabIndex === i ? 'active' : ''}`}
-						key={t.id}
-						onClick={() => setTabIndex(i)}
-					>{t.name}</div>
-				))}
-				<div className="spacer" />
-				<div className="right-nav">
-					<a className="icon" href="https://github.com/dan9418/play-what-sandbox" target="_blank"><Icons.GitHub /></a>
-					<div className={`meatball pw-hov`} onClick={() => null}>
-						<Icons.Menu />
-					</div>
-				</div>
-			</nav>
-			<div className="stage">
-				<div className="stage-content">
-					{tab.component}
-				</div>
-			</div>
+			Hello World!
 		</>
 	);
 };
