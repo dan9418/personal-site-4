d7fa8160c26a4eae4fe258592ca55d2f6cd1aa11
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-12-04 18:00:18 -0800

    style menu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,6 +12,11 @@ import Resume from "./pages/Resume/Resume";
 
 const APP_PAGES = [
 	{
+		id: 'home',
+		name: 'Home',
+		component: Resume
+	},
+	{
 		id: 'resume',
 		name: 'Resume',
 		component: Resume
@@ -66,28 +71,32 @@ const getRoutes = () => {
 	return routes;
 }
 
-function getPageLinks() {
+const getPageLinks = () => {
 	let pageLinks = [];
 	for (let i = 0; i < APP_PAGES.length; i++) {
 		let page = APP_PAGES[i];
 		let path = `/${page.id}`;
 		pageLinks.push(
-			<Link key={page.id} to={path}>
-				{page.name}
-			</Link>
+			<li>
+				<Link key={page.id} to={path}>
+					{page.name}
+				</Link>
+			</li>
 		);
 	}
 	return pageLinks;
 }
 
-function getExternalLinks() {
+const getExternalLinks = () => {
 	let externalLinks = [];
 	for (let i = 0; i < APP_LINKS.length; i++) {
 		let link = APP_LINKS[i];
 		externalLinks.push(
-			<a href={link.href}>
-				{link.name}
-			</a>
+			<li>
+				<a href={link.href}>
+					{link.name}
+				</a>
+			</li>
 		);
 	}
 	return externalLinks;
@@ -97,13 +106,30 @@ const Main = () => {
 	return (
 		<Router>
 			<nav>
-				{getPageLinks()}
-				{getExternalLinks()}
+				<section>
+					<img src='/assets/me.jpg' />
+					<label>Dan Bednarczyk</label>
+					<div className="caption">Engineer ‧ Artist ‧ Activist</div>
+					<div className="location-wrapper">Sacramento, CA</div>
+				</section>
+				<section>
+					<ul>
+						{getPageLinks()}
+					</ul>
+				</section>
+				<div className="list-header">LINKS</div>
+				<section>
+					<ul>
+						{getExternalLinks()}
+					</ul>
+				</section>
 			</nav>
-			<Switch>
-				{getRoutes()}
-				<Redirect key='default' to="/resume" />
-			</Switch>
+			<main>
+				<Switch>
+					{getRoutes()}
+					<Redirect key='default' to="/resume" />
+				</Switch>
+			</main>
 		</Router>
 	);
 };
